3f75fc5c7f760886e86d22a47ef957f1ae385435
Manuelinux <manuelinux@gmail.com>
2022-04-01 17:32:35 -0400

    Joses review 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000; // use either the host env var port (PORT
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
   fetch('https://www.boredapi.com/api/activity') // fetch activity from bored API - https://www.boredapi.com/about
     .then(res => res.json()) // return a promise containing the response
-    .then(json => res.send(`<h1>Hi Jose Today's Activity: ${json.activity}!</h1>`)) // extract the JSON body content from the response (specifically the activity value) and sends it to the client
+    .then(json => res.send(`<h1>Hi Jose  Muñoz Today's Activity: ${json.activity}!</h1>`)) // extract the JSON body content from the response (specifically the activity value) and sends it to the client
     .catch(function(err){ // catch any errors
       console.log(err); // log errors to the console
     })
